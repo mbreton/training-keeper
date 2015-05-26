a7cefb396ce98c52a753cbfd4363de19c487a582
Mathieu Breton <mbreton@xebia.fr>
2015-05-26 18:37:23 +0200

    Test CD pipeline

--- app/controllers/Training.scala
+++ app/controllers/Training.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 object Training extends Controller {
 
   def list = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Test CD pipeline"))
   }
 
 }
